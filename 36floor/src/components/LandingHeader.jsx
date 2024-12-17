b302840d502b5d30f6295e375da9a7b6bfec5c57
Miguel Seruntine <miguelseruntine@gmail.com>
2024-12-17 15:27:34 -0600

    new file addition

--- 36floor/src/components/LandingHeader.jsx
+++ 36floor/src/components/LandingHeader.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import the36floor from 'assets/the36floor.JPG'
+import reactLogo from './assets/react.svg'
 
 function LandingHeader() {
   const [count, setCount] = useState(0)
@@ -8,6 +10,8 @@ function LandingHeader() {
       <p className="read-the-docs">
         This is the landing header!
       </p>
+      <img src={the36floor} className="logo react" alt="React logo" />
+      <img src={reactLogo} className="logo react" alt="React logo" />
     </>
   )
 }
