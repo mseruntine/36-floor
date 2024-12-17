2f17f133ed6f0dd3c4d788f95174ed8a5e04f68f
Miguel Seruntine <miguelseruntine@gmail.com>
2024-12-17 16:19:16 -0600

    edits to header

--- 36floor/src/components/LandingHeader.jsx
+++ 36floor/src/components/LandingHeader.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import the36floor from 'assets/the36floor.JPG'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
+import the36floor from '../assets/the36floor.JPG';
+import reactLogo from '../assets/react.svg';
+import './LandingHeader.css'
 
 function LandingHeader() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  console.log(the36floor, reactLogo); // Debugging
 
   return (
     <>
-      <p className="read-the-docs">
-        This is the landing header!
-      </p>
-      <img src={the36floor} className="logo react" alt="React logo" />
-      <img src={reactLogo} className="logo react" alt="React logo" />
+      {/* <p className="read-the-docs">This is the landing header!</p> */}
+      <img src={the36floor} className="theLogo" alt="The 36th Floor" />
     </>
-  )
+  );
 }
 
-export default LandingHeader
+export default LandingHeader;
