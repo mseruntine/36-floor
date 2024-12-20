5c2e9e2530f291a030053f4d7b1b6f9333db8f5b
Miguel Seruntine <miguelseruntine@gmail.com>
2024-12-19 18:38:46 -0600

    additions to landing header

--- 36floor/src/components/LandingHeader.jsx
+++ 36floor/src/components/LandingHeader.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import the36floor from '../assets/the36floor.JPG';
 import reactLogo from '../assets/react.svg';
 import './LandingHeader.css'
-import { Text } from "@chakra-ui/react"
+import { Flex, Text, Image } from "@chakra-ui/react"
 
 function LandingHeader() {
   const [count, setCount] = useState(0);
@@ -11,8 +11,12 @@ function LandingHeader() {
 
   return (
     <>
-      <img src={the36floor} className="theLogo" alt="The 36th Floor" />
-      <Text>shaking up the underground</Text>
+        <Flex w="50%" justify="center" align="center" textAlign="center" align-items="center" direction="column">
+          <Image src={the36floor}></Image>
+        </Flex>
+        <Flex w="100%" textAlign="center">
+          <Text color="black" >shaking up the underground</Text>
+        </Flex>
     </>
   );
 }
